7c7474f993a42b7f80b9576b15e8c5bba75ba30f
Mathias Langlet <mathias.langlet@gmail.com>
2023-07-05 13:10:26 +0200

    Add nvim config

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,13 @@
+vim.opt.showcmd = true
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2 
+
+-- Check if I want these 
+-- vim.g.mapleader = ' '
+-- vim.g.maplocalleader = ' '
+-- vim.opt.backspace = '2'
+-- vim.opt.laststatus = 2
+-- vim.opt.autowrite = true
+-- vim.opt.autoread = true
